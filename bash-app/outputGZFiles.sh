23ccd522ebaac1986c0dd13d5f320a262a642d0d
Your Name <you@example.com>
2021-08-24 13:44:23 +0200

    bash-app: Updated the bash script

--- bash-app/outputGZFiles.sh
+++ bash-app/outputGZFiles.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 srcURL='http://ftp.debian.org/debian/dists/jessie/Release'
-regex = "^(.gz)$"
+regex="(.gz)$"
 counter=0
 
-if [[ -f $(command -v curl) ]]; then
+if [[ -z $(command -v curl) ]]; then
     echo "Error: curl not found -- exiting"
     exit 1
 else 
     curlOutput=$(curl -s ${srcURL}) || echo "Error: curl unable to complete request" && \
-        echo ${curlOutput}  |grep -E "${regex}" |awk '{print "["NR"] Checksum: " $1 " FileName: "$3}'
-done
+        curl -s ${srcURL}  |grep -E "${regex}" |awk '{print "["NR"] Checksum: " $1 " FileName: "$3}'
+fi
